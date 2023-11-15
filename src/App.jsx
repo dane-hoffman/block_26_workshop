3c9ce3e1ed110a4232891b507cca5ed57146223d
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-15 13:45:25 -0700

    Console logged the dummy contact list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
 import ContactList from './components/ContactList'
+import { dummyContacts } from './components/ContactList'
 
 export default function App() {
+  const [contacts, setContacts] = useState(dummyContacts)
+  console.log("Contacts: ", contacts)
   return (
     <>
       <ContactList />
